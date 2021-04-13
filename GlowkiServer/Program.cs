7d9a136c27831415881d627112024f679dd722f6
Michał Zembik <michal.zembik@soneta.pl>
2021-04-14 01:14:59 +0200

    walka z ip

--- GlowkiServer/Program.cs
+++ GlowkiServer/Program.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Hosting;
 
 namespace GlowkiServer
@@ -22,6 +24,13 @@ namespace GlowkiServer
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.ConfigureKestrel(options =>
+                    {
+                        options.Listen(IPAddress.Parse("146.59.18.155"), 5001, listenOptions =>
+                        {
+                            //listenOptions.Protocols = HttpProtocols.Http2;
+                        });
+                    });
                     webBuilder.UseStartup<Startup>();
                 });
     }
